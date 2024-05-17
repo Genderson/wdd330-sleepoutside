e2243daadd07f34d6b3fc7ea8f32413fceb4451d
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-05-17 12:45:52 -0500

    changes to implement same discount logic

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,40 +1,30 @@
 import { getData } from "./productData.mjs";
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate, calculateDiscount } from "./utils.mjs";
 
 export async function productList(catagory, selector) {
     // get the element we will insert the list into from the selector
 
     // get the list of products 
     const productList = await getData(catagory);
-    //return productList; -- testing
+
     const filteredList = productList.slice(0,4);
     // render out the product list to the element
-    // renderList(productList);
-    const element = document.querySelector(".product-list");
-    renderListWithTemplate(productCardTemplate,element, filteredList);
+    renderListWithTemplate(productCardTemplate, selector, filteredList);
 }
 
 export function productCardTemplate(product) {
-    let productDiscountPercentage = `${(
-        100 *
-        (1 - product.FinalPrice / product.ListPrice)
-      ).toFixed(0)}%`;
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
-    <img
-      src="${product.Image}"
-      alt="Image of ${product.Name}"
-    />
-    <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.NameWithoutBrand}</h2>
-    <span class="product-card__price">$${product.FinalPrice}</span>
-    <span class="discount">SAVE ${productDiscountPercentage}</span>
-    <span class="list-price">$${product.ListPrice}</span></a>
-    </li>`;
-}
+  const productDiscountPercentage = calculateDiscount(product.FinalPrice, product.ListPrice);
 
-export function renderList(productList) {
-    //console.table(productList) -- testing
-    const htmlItems = productList.map(product => productCardTemplate(product))
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.NameWithoutBrand}</h2>
+  <p class="product-card__price">Final Price: $${product.FinalPrice}</p>
+  <span class="discount">SAVE ${productDiscountPercentage}</span>
+  <p class="list-price">Original Price: $${product.ListPrice}</p></a>
+  </li>`;
 }
