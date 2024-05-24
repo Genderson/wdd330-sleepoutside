a20ac348fa9c354a3fb830965253425307e2407b
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-05-23 21:23:09 -0500

    resolve issue loading the item carts number

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,26 @@ import { productList } from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 import { displayTotalCartItems } from "./totalCartItems.mjs";
 
-await loadHeaderFooter();
+//loadHeaderFooter();
+/* 
+try {
+    await loadHeaderFooter();
+    displayTotalCartItems();
+  } catch (error) {
+    console.error(error);
+  }*/
+
+(async () => {
+  try {
+    await loadHeaderFooter();
+    displayTotalCartItems();
+  } catch (error) {
+    //console.error(error);
+  }
+})();
 
 const element = document.querySelector(".product-list");
 
 if (element) {
-    productList("tents", element);
+  productList("tents", element);
 }
-
-displayTotalCartItems();
