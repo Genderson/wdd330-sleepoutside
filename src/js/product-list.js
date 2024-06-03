14c59c6f743bd9355b418c849f89ae1606e7b481
anderson-robertp <anderon-rp@hotmail.com>
2024-06-02 23:39:57 -0500

    fixed and wrking search and sort

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,8 +1,16 @@
 import { productList, getCategoryType } from "./productList.mjs";
+import { setClick } from "./utils.mjs";
 
 const element = document.querySelector(".product-list");
+const sortOptions = document.getElementById("sortOptions");
 
+// initial display
 if (element) {
-  productList(element);
+  productList(element, "nameasc");
   getCategoryType();
 }
+
+sortOptions.addEventListener("change", function(){
+  const sortOrder = this.value;
+  productList(element, sortOrder);
+});
