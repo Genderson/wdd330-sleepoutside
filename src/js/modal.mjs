f8bc459316fde4cb52c39872ac176529a0b8a1f7
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-06-08 00:01:27 -0500

    view lookup functionality

---
+++ src/js/modal.mjs
@@ -0,0 +1,13 @@
+export function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
+    }
+}
+
+export function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
