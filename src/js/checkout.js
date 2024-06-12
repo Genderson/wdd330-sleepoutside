b4109c4320929420a6e87fae68496e16e5883342
anderson-robertp <anderon-rp@hotmail.com>
2024-06-11 21:54:52 -0500

    Week 7 changes

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -11,11 +11,16 @@ if (zip != null) {
 }
 
 const form = document.forms[0];
+
 if (form != null) {
   // this is how it would look if we listen for the submit on the form
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  form.checkoutSubmit.addEventListener("click", (e) => {
+     e.preventDefault();
     // e.target would contain our form in this case
-    checkoutProcess.checkout(e.target);
+    var chk_status = form.checkValidity();
+    form.reportValidity();
+    if (chk_status) {
+      checkoutProcess.checkout(form);
+    }
   });
 }
