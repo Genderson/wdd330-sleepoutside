cee4bfd8ede05f94d8976ed3563243db8f3fa919
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-06-14 19:27:35 -0500

    carousel added in the images detail page

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,14 +1,13 @@
 import { findProductById } from "./externalServices.mjs";
 import { setLocalStorage, getLocalStorage, calculateDiscount } from "./utils.mjs";
 import { displayTotalCartItems } from "./totalCartItems";
+import { loadImages } from "./carousel.mjs";
 
 let product = {};
 
 export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
   product = await findProductById(productId);
-  // console.log(product);
-
   renderProductDetails(productId);
 
   document.getElementById("addToCart").addEventListener("click", addToCart);
@@ -90,11 +89,73 @@ export function clearViewProductDetails() {
 
 function renderProductDetails(productId, productQuantity = 1) {
   if (product) {
-    const productDiscountPercentage = calculateDiscount(product.FinalPrice, product.ListPrice);
+        document.querySelector("#productCategory").innerText = `${product.Category.charAt(0).toUpperCase()}${product.Category.slice(1)}`;   
+        document.querySelector("#productName").innerText = product.Brand.Name;
+        document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
+      if(product.Images.ExtraImages == null) {
 
-    console.log(product);
-    // Reference to capitalize first letter of a string:
-    // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+        // Reference for window.screen.width: https://developer.mozilla.org/en-US/docs/Web/API/Screen/width
+        if (window.screen.width >= 500) {
+          document.querySelector("#productImage").src = product.Images.PrimaryExtraLarge;
+        }
+        else if (window.screen.width >= 250) {
+          document.querySelector("#productImage").src = product.Images.PrimaryLarge;
+        }
+        else {
+          document.querySelector("#productImage").src = product.Images.PrimaryMedium;
+        }
+    
+        // Reference for window.addEventListener and resize: https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
+        window.addEventListener("resize", () => {
+          if (window.screen.width >= 500) {
+            document.querySelector("#productImage").src = product.Images.PrimaryExtraLarge;
+          }
+          else if (window.screen.width >= 250) {
+            document.querySelector("#productImage").src = product.Images.PrimaryLarge;
+          }
+          else {
+            document.querySelector("#productImage").src = product.Images.PrimaryMedium;
+          }   
+        });
+
+        document.querySelector("#productImage").alt = product.Name;
+    }
+    else{
+      let imageArr = [];
+      imageArr.push({"Title": "PrimaryExtraLarge", "Src": product.Images.PrimaryExtraLarge});
+      
+      product.Images.ExtraImages.forEach((image) => {
+        imageArr.push(image);
+      });
+
+      loadImages(imageArr);
+    }
+
+    const productDiscountPercentage = calculateDiscount(product.FinalPrice, product.ListPrice);
+    document.querySelector("#productQuantity").value = productQuantity;
+    document.querySelector("#productFinalPrice").innerText = `$${product.FinalPrice}`;
+    document.querySelector("#discountPercentage").innerText = `SAVE ${productDiscountPercentage}`;
+    document.querySelector("#discountPrice").innerText = `$${product.ListPrice - product.FinalPrice}`;
+    document.querySelector("#originalPrice").innerText = `$${product.ListPrice}`;
+
+    document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
+
+    const cartTotal = document.querySelector("#addToCart");
+    cartTotal.classList.remove("hide");
+  } else {
+    const cartTotal = document.querySelector("#addToCart");
+    cartTotal.classList.add("hide");
+
+    const message = document.querySelector("#error-message");
+    message.textContent = `Product ${productId} does not exist`;
+    message.classList.remove("hide");
+  }
+}
+
+function viewProductDetails(product) {
+    const productDiscountPercentage = calculateDiscount(product.FinalPrice, product.ListPrice);
     document.querySelector("#productCategory").innerText = `${product.Category.charAt(0).toUpperCase()}${product.Category.slice(1)}`;   
     document.querySelector("#productName").innerText = product.Brand.Name;
     document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
@@ -124,59 +185,6 @@ function renderProductDetails(productId, productQuantity = 1) {
     });
     
     document.querySelector("#productImage").alt = product.Name;
-
-    document.querySelector("#productQuantity").value = productQuantity;
-    document.querySelector("#productFinalPrice").innerText = `$${product.FinalPrice}`;
-    document.querySelector("#discountPercentage").innerText = `SAVE ${productDiscountPercentage}`;
-    document.querySelector("#discountPrice").innerText = `$${product.ListPrice - product.FinalPrice}`;
-    document.querySelector("#originalPrice").innerText = `$${product.ListPrice}`;
-
-    document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
-    document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
-    document.querySelector("#addToCart").dataset.id = product.Id;
-
-    const cartTotal = document.querySelector("#addToCart");
-    cartTotal.classList.remove("hide");
-  } else {
-    const cartTotal = document.querySelector("#addToCart");
-    cartTotal.classList.add("hide");
-
-    const message = document.querySelector("#error-message");
-    message.textContent = `Product ${productId} does not exist`;
-    message.classList.remove("hide");
-  }
-}
-
-function viewProductDetails(product) {
-    const productDiscountPercentage = calculateDiscount(product.FinalPrice, product.ListPrice);
-    document.querySelector("#productCategory").innerText = `${product.Category.charAt(0).toUpperCase()}${product.Category.slice(1)}`;   
-    document.querySelector("#productName").innerText = product.Brand.Name;
-    document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
-        // Reference for window.screen.width: https://developer.mozilla.org/en-US/docs/Web/API/Screen/width
-        if (window.screen.width >= 500) {
-          document.querySelector("#productImage").src = product.Images.PrimaryExtraLarge;
-        }
-        else if (window.screen.width >= 250) {
-          document.querySelector("#productImage").src = product.Images.PrimaryLarge;
-        }
-        else {
-          document.querySelector("#productImage").src = product.Images.PrimaryMedium;
-        }
-    
-        // Reference for window.addEventListener and resize: https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
-        window.addEventListener("resize", () => {
-          if (window.screen.width >= 500) {
-            document.querySelector("#productImage").src = product.Images.PrimaryExtraLarge;
-          }
-          else if (window.screen.width >= 250) {
-            document.querySelector("#productImage").src = product.Images.PrimaryLarge;
-          }
-          else {
-            document.querySelector("#productImage").src = product.Images.PrimaryMedium;
-          }   
-        });
-        
-        document.querySelector("#productImage").alt = product.Name;
 
     document.querySelector("#productFinalPrice").innerText = `$${product.FinalPrice}`;
     document.querySelector("#discountPercentage").innerText = `SAVE ${productDiscountPercentage}`;
