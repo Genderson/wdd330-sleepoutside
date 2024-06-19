daf1165e1770d2dcde46fa87436f4f4ddaeb7e29
John T <jonathant4242@gmail.com>
2024-06-18 19:37:18 -0700

    jt--team08

---
+++ src/js/login.js
@@ -0,0 +1,30 @@
+import { getParam, setLocalStorage } from "./utils.mjs";
+import { loginRequest } from "./externalServices.mjs";
+import { jwtDecode } from "jwt-decode";
+
+
+const redirect = getParam("redirect");
+
+
+const form = document.forms[0];
+
+if (form != null) {
+  // this is how it would look if we listen for the submit on the form
+  form.loginButton.addEventListener("click", (e) => {
+     e.preventDefault();
+    // e.target would contain our form in this case
+    var chk_status = form.checkValidity();
+    form.reportValidity();
+    if (chk_status) {
+        const email = form.email.value;
+        const password = form.password.value;
+        login(email, password, redirect);
+    }
+  });
+}
+async function login(email, password, redirect = "/") {
+const credentials = { email: email , password: password };
+const response = await loginRequest(credentials);
+setLocalStorage("so_token", response);
+window.location = redirect;
+}
