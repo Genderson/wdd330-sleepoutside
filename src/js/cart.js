f328429368b3c99c8c8bea1661f5ca85d8c4db3e
DallinS <dallinstephens1@gmail.com>
2024-06-21 08:08:20 -0600

    Dallin wish-list save for later cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const productList = document.querySelector(".product-list-cart");
   // When button is clicked, looks for the closest button to click
   // then loks for the closest item to the button
+
   productList.addEventListener("click", (event) => {
     if (event.target.closest(".remove-item")) {
       const cartItem = event.target.closest(".cart-item");
@@ -18,10 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
     else if (event.target.closest(".save-item")) {
       const cartItem = event.target.closest(".cart-item");
       const itemId = cartItem.dataset.id; // Use 'id' in lowercase
-      console.log(cartItem);
-      console.log(itemId);
-      //removeFromCart(itemId); // Function to remove item from local storage
-      //cartItem.remove(); // Remove the item from the DOM directly
+      wishList.append(cartItem);
+      document.getElementById(itemId).textContent = "Delete";
+      document.getElementById(`${itemId}-save-move`).textContent = "Move to cart";
+      // Get Cart item from local storage
+      let cart = getLocalStorage("so-cart");
+      console.log(cart);
+
+      //Find the item in the index
+      const itemIndex = cart.findIndex((item) => item.Id === itemId);
+      // Reference to change DOM image src: https://www.w3schools.com/jsref/prop_img_src.asp
+      document.getElementById(`${itemId}-image`).src = cart[itemIndex].Images.PrimarySmall;
+      saveForLater(cart[itemIndex], wishList);
+      removeFromCart(itemId); // Function to remove item from local storage
+
+      document.getElementById(itemId).addEventListener("click", () => {
+        const cartItem = event.target.closest(".cart-item");
+        cartItem.remove(); // Remove the item from the DOM directly
+        removeFromSaveForLater(itemId); // Function to remove item from local storage
+      });
     }    
   });
 
@@ -42,16 +58,39 @@ document.addEventListener("DOMContentLoaded", () => {
     displayTotalCartItems();
     sumTotalItems(getLocalStorage("so-cart") || []); // Update the total
   }
+  const wishList = document.querySelector("#wish-list");
+  let saveForLaterHTML = localStorage.getItem("save-for-later-html");
+  if (saveForLaterHTML != null) {
+    wishList.innerHTML = saveForLaterHTML;
+  }
+
 });
 
 shoppingCart();
 displayTotalCartItems();
 sumTotalItems(getLocalStorage("so-cart") || []);
 
-function saveForLater(item) {
-  let cartItems = getLocalStorage("save-for-later-cart") || [];
-  console.log(cartItems);
-  cartItems.push(item);
+function saveForLater(item, wishList) {
+  let saveForLaterItems = getLocalStorage("save-for-later-cart") || [];
+  console.log(saveForLaterItems);
+  saveForLaterItems.push(item);
 
-  setLocalStorage("saved-for-later-cart", cartItems);
+  localStorage.setItem("save-for-later-cart", JSON.stringify(saveForLaterItems));
+  // Reference to store an html element in localStorage: https://stackoverflow.com/questions/48239869/how-to-store-a-complete-div-in-localstorage
+  localStorage.setItem("save-for-later-html", wishList.innerHTML);
+}
+
+function removeFromSaveForLater(itemId) {
+  // Get Cart items from local storage
+  let cart = getLocalStorage("save-for-later-cart") || [];
+
+  //Find the item in the index
+  const itemIndex = cart.findIndex((item) => item.Id === itemId);
+
+  // Remove from array
+  if (itemIndex !== -1) {
+    cart.splice(itemIndex, 1);
+    // update localStorage
+    localStorage.setItem("save-for-later-cart", JSON.stringify(cart));
+  }
 }
--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -29,9 +29,9 @@ export function cartItemTemplate(item) {
 
   const cartItem = `<li class="cart-item" data-id=${item.Id}>
   <button class="remove-item"><span id=${item.Id}>❎</span></button>
-  <button class="save-item"><span id=${item.Id}>Save for later</span></button>
+  <button class="save-item"><span id=${item.Id}-save-move>Save for later</span></button>
   <a href="/product_pages/index.html?product=${item.Id}" class="cart-card__image">
-    <img
+    <img id=${item.Id}-image
       src="${imageSizeUrl}"
       alt="${item.Name}"
     />
